229d6abb5338d2d40c9605245e381297a29aed60
Dylan Sanderson <dylan.sanderson@students.hevs.ch>
2026-02-08 10:42:50 +0100

    firebase 2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,13 +3,16 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.whateka"
+    namespace = "com.shz.whateka"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +27,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.whateka"
+        applicationId = "com.shz.whateka"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
