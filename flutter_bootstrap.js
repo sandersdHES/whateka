d90cb82b082ca29aeb9194e3a16c0876d5e37ca4
YannZ1744 <YannZ1744@users.noreply.github.com>
2026-05-05 15:14:22 +0000

    Deploy: Merge pull request #15 from yzurbuchen-source/main
    feat(profile): personnalisation avatar en stand-by 3dbc39c44ae0befb794fca2e3dad2e9e0a3801f2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1429331202" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3002379889" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
