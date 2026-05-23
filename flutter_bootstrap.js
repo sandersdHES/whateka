52d56dc2393f0a1d7e3ea253b6c3314b90ec82a4
YannZ1744 <YannZ1744@users.noreply.github.com>
2026-05-23 10:22:12 +0000

    Deploy: Merge pull request #28 from yzurbuchen-source/feat/favorites-list-map-toggle
    feat(favorites): toggle Liste / Carte pour voir ses favoris aa110d7b704bd02f21203c24eaa03cf91586859b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "500852421" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1474719245" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
