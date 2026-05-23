095ab12b24816b0c25869958a908ad338b49947c
YannZ1744 <YannZ1744@users.noreply.github.com>
2026-05-23 10:16:47 +0000

    Deploy: Merge pull request #27 from yzurbuchen-source/feat/thanks-for-interest-promo-wa2026
    feat(subscription): écran 'Merci' + code promo WA2026 (3 mois Évasion offerts) 12d4a8091cee6afe93641cff514e9d257d583e15

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1996090350" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "500852421" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
