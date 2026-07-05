9d5aee643e051a8de6de9e5aa23f2d88aaa4e901
sandersdHES <sandersdHES@users.noreply.github.com>
2026-07-05 16:14:19 +0000

    Deploy: docs: merge bugs and improvements tracking document into main bbc5c20cccb62d1ccb6e843107b6d46fdcf0a966

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "239037781" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3949209184" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
